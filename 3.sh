b7e7dd316486ac7c318472d2090c5f611e353eaf
joornxonzus <yxxzyy6280@163.com>
2021-04-19 15:33:41 +0800

    1120-dt2 Signed-off-by: LeeBin <hest0001@163.com>

--- 3.sh
+++ 3.sh
@@ -1,4 +1,5 @@
-dt=$(date)
+dt=$(date +%m%d)
+dt2=$(date +%H%M)
 git add .
-git commit -m "$dt\n Signed-off-by: LeeBin <hest0001@163.com>"
+git commit -m "$dt-dt2 Signed-off-by: LeeBin <hest0001@163.com>"
 git push origin master
